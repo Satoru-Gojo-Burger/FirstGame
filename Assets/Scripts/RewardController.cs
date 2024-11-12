66f0a3220b156260d0762019f4fe6de8e1870f41
Satoru-Gojo-Burger <grah228@yandex.ru>
2024-11-12 13:48:59 +0300

    RewardUpd

--- Assets/Scripts/RewardController.cs
+++ Assets/Scripts/RewardController.cs
@@ -6,16 +6,72 @@ namespace Golf
 {
     public class RewardController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public List<GameObject> Rewards;
+
+        public void Zero()
         {
-        
+            GameInstance.score = 0;
+        }
+        public void oerZ()
+        {
+            GameInstance.score = 30;
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            if (GameInstance.score >= 5)
+            {
+                Rewards[0].SetActive(true);
+            }
+            else
+            {
+                Rewards[0].SetActive(false);
+            }
+
+            if (GameInstance.score >= 10)
+            {
+                Rewards[1].SetActive(true);
+            }
+            else
+            {
+                Rewards[1].SetActive(false);
+            }
+
+            if (GameInstance.score >= 15)
+            {
+                Rewards[2].SetActive(true);
+            }
+            else
+            {
+                Rewards[2].SetActive(false);
+            }
+
+            if (GameInstance.score >= 20)
+            {
+                Rewards[3].SetActive(true);
+            }
+            else
+            {
+                Rewards[3].SetActive(false);
+            }
+
+            if (GameInstance.score >= 25)
+            {
+                Rewards[4].SetActive(true);
+            }
+            else
+            {
+                Rewards[4].SetActive(false);
+            }
+
+            if (GameInstance.score >= 30)
+            {
+                Rewards[5].SetActive(true);
+            }
+            else
+            {
+                Rewards[5].SetActive(false);
+            }
         }
     }
 }
